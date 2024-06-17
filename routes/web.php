955e6573ef8998bfc2274cce5fd4ee65ddf8539f
Andrei Bochkov <auslendaer@gmail.com>
2024-06-17 21:24:03 +0000

    flow

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NarratorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('narrator', function () {
+    return view('narrator');
+});
+Route::post('narrator', [NarratorController::class, 'narrate'])->name('narrate');
